09c9a281eb56674ff28b9c36657c411bea9bd185
Wei Hang Hong <weihanghong2001@gmail.com>
2024-07-04 23:05:47 -0400

    initialize project

---
+++ src/main/java/com/hong/css_adventure/CssAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.hong.css_adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CssAdventureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hong/css_adventure/CssAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.css_adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
